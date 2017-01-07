ea588de08f2d4cc294936b0a3f221012d2f251e6
LEOnardo Ma <LEO@LEOnardos-MacBook-Pro.local>
2017-01-07 14:05:36 -0800

    change email addresss

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(!$error)
 $subject ="New message!";
 $message ="New request!\n\nE-mail: ".$email."\n\nName: " .$name."\n\nMessage:".$msg."\n\n";
 
-$mail = mail("info@idealfuture.ca", $subject, $message,
+$mail = mail("leoma34@hotmail.com", $subject, $message,
 
 "From: ".$name." <".$email."> "."Reply-To: ".$email." "." X-Mailer: PHP/" . phpversion());
 if($mail)
